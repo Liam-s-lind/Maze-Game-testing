a934b4096e3943da14dd4f5f4e76b5a1f4464ae4
Liam-s-lind <>
2023-11-02 02:54:31 +0000

    added DrawMaze function and related drawing methods

--- assets/script.js
+++ assets/script.js
@@ -229,3 +229,108 @@ function defineStartEnd() {
             break;
     }
 }
+
+// This function takes a maze ('Maze'), a canvas context ('ctx'), a cell size ('cellsize'), and an optional end sprite ('endSprite') and draws the maze on the canvas.
+function DrawMaze(Maze, ctx, cellsize, endSprite = null) {
+    var map = Maze.map();
+    var cellSize = cellsize;
+    var drawEndMethod;
+    ctx.lineWidth = cellSize / 40;
+
+    // This function allows redrawing the maze with a different cell size.
+    this.redrawMaze = function(size) {
+        cellSize = size;
+        ctx.lineWidth = cellSize / 50;
+        drawMap();
+        drawEndMethod();
+    };
+
+    // This function draws the walls and paths of individual cells.
+    function drawCell(xCord, yCord, cell) {
+        var x = xCord * cellSize;
+        var y = yCord * cellSize;
+
+        // Draw walls based on cell properties (n, s, e, w).
+        if (cell.n == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + cellSize, y);
+            ctx.stroke();
+        }
+        if (cell.s === false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.e === false) {
+            ctx.beginPath();
+            ctx.moveTo(x + cellSize, y);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.w === false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.stroke();
+        }
+    }
+
+    // This function draws the entire maze by iterating through cells and drawing each one.
+    function drawMap() {
+        for (x = 0; x < map.length; x++) {
+            for (y = 0; y < map[x].length; y++) {
+                drawCell(x, y, map[x][y]);
+            }
+        }
+    }
+
+    // This function draws an end flag at the maze's exit point.
+    function drawEndFlag() {
+        var coord = Maze.endCoord();
+        var gridSize = 4;
+        var fraction = cellSize / gridSize - 2;
+        var colorSwap = true;
+        for (let y = 0; y < gridSize; y++) {
+            if (gridSize % 2 == 0) {
+                colorSwap = !colorSwap;
+            }
+            for (let x = 0; x < gridSize; x++) {
+                ctx.beginPath();
+                ctx.rect(
+                    coord.x * cellSize + x * fraction + 4.5,
+                    coord.y * cellSize + y * fraction + 4.5,
+                    fraction,
+                    fraction
+                );
+                if (colorSwap) {
+                    ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+                } else {
+                    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+                }
+                ctx.fill();
+                colorSwap = !colorSwap;
+            }
+        }
+    }
+
+    // This function draws an end sprite at the maze's exit point if provided.
+    function drawEndSprite() {
+        var offsetLeft = cellSize / 50;
+        var offsetRight = cellSize / 25;
+        var coord = Maze.endCoord();
+        ctx.drawImage(
+            endSprite,
+            2,
+            2,
+            endSprite.width,
+            endSprite.height,
+            coord.x * cellSize + offsetLeft,
+            coord.y * cellSize + offsetLeft,
+            cellSize - offsetRight,
+            cellSize - offsetRight
+        );
+    }
+}
+
