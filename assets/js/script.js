a17413edcb451504333b35ac5171149fe60b8ba7
Liam-s-lind <>
2023-11-02 03:57:32 +0000

    setup window load and resize handlers, sprite loading and modification, and maze initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -552,4 +552,83 @@ var maze, draw, player;
 var cellSize;
 var difficulty;
 
+// This function sets up event handlers when the window loads.
+window.onload = function() {
+    // Determine the dimensions of the 'view' element and adjust the canvas size accordingly.
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+        ctx.canvas.width = viewHeight - viewHeight / 100;
+        ctx.canvas.height = viewHeight - viewHeight / 100;
+    } else {
+        ctx.canvas.width = viewWidth - viewWidth / 100;
+        ctx.canvas.height = viewWidth - viewWidth / 100;
+    }
 
+    // Load and modify sprites, ensuring both are complete before proceeding.
+    var completeOne = false;
+    var completeTwo = false;
+    var isComplete = () => {
+        if (completeOne === true && completeTwo === true) {
+            console.log("Runs");
+            setTimeout(function(){
+                makeMaze();
+            }, 500);
+        }
+    };
+    
+    sprite = new Image();
+    sprite.src = "./key.png" + "?" + new Date().getTime();
+    sprite.setAttribute("crossOrigin", " ");
+    sprite.onload = function() {
+        sprite = changeBrightness(1.2, sprite);
+        completeOne = true;
+        console.log(completeOne);
+        isComplete();
+    };
+
+    finishSprite = new Image();
+    finishSprite.src = "./home.png" + "?" + new Date().getTime();
+    finishSprite.setAttribute("crossOrigin", " ");
+    finishSprite.onload = function() {
+        finishSprite = changeBrightness(1.1, finishSprite);
+        completeTwo = true;
+        console.log(completeTwo);
+        isComplete();
+    };
+};
+
+// This function adjusts canvas size when the window is resized.
+window.onresize = function() {
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+        ctx.canvas.width = viewHeight - viewHeight / 100;
+        ctx.canvas.height = viewHeight - viewHeight / 100;
+    } else {
+        ctx.canvas.width = viewWidth - viewWidth / 100;
+        ctx.canvas.height = viewWidth - viewWidth / 100;
+    }
+    cellSize = mazeCanvas.width / difficulty;
+    if (player != null) {
+        draw.redrawMaze(cellSize);
+        player.redrawPlayer(cellSize);
+    }
+};
+
+// This function creates the maze and initializes game elements.
+function makeMaze() {
+    if (player != undefined) {
+        player.unbindKeyDown();
+        player = null;
+    }
+    var e = document.getElementById("diffSelect");
+    difficulty = e.options[e.selectedIndex].value;
+    cellSize = mazeCanvas.width / difficulty;
+    maze = new Maze(difficulty, difficulty);
+    draw = new DrawMaze(maze, ctx, cellSize, finishSprite);
+    player = new Player(maze, mazeCanvas, cellSize, displayVictoryMess, sprite);
+    if (document.getElementById("mazeContainer").style.opacity < "100") {
+        document.getElementById("mazeContainer").style.opacity = "100";
+    }
+}
